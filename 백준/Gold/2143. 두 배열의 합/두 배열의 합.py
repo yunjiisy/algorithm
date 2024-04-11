7f9d3e5d4d774d328f0d93adfc2dca0a275def9b
yunji <78731810+yunjiisy@users.noreply.github.com>
2024-04-12 00:20:31 +0900

    [Gold III] Title: 두 배열의 합, Time: 4052 ms, Memory: 71064 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
@@ -1,4 +1,4 @@
-import bisect
+from bisect import bisect_left, bisect_right
 
 T = int(input())  # 부 배열의 합으로 만족되야 하는 값
 n = int(input())  # 배열 A의 원소 개수
@@ -7,22 +7,20 @@ m = int(input())  # 배열 B의 원소 개수
 B = list(map(int, input().split()))  # 배열 B
 
 result = 0  # 출력할 값 초기화
-Asum, Bsum = A, B  # 각 배열로 초기화
+# a -> A의 부 배열의 합 배열, b-> B의 부 배열의 합 배열
+a, b = A, B  # 각 배열로 초기화 (요소 하나인 부 배열의 합)
 for i in range(n-1):
     for j in range(i + 1, n):
-        # print(i)
-        # print(j)
-        Asum.append(sum(A[i:j + 1]))  # 배열 A의 모든 부배열의 합을 추가해준다.
-        # print(Asum)
-for s in range(m):
-    for e in range(s + 1, m):
-        Bsum.append(sum(B[s:e + 1]))  # 배열 A의 모든 부배열의 합을 추가해준다.
+        a.append(sum(A[i:j + 1]))  # 배열 A의  부 배열의 합을 추가
+for i in range(m-1):
+    for j in range(i + 1, m):
+        b.append(sum(B[i:j + 1]))  # 배열 B의  부 배열의 합을 추가
 
-Asum.sort();
-Bsum.sort()
-
-for i in range(len(Asum)):
-    l = bisect.bisect_left(Bsum, T - Asum[i])
-    r = bisect.bisect_right(Bsum, T - Asum[i])
+# a.sort();
+b.sort() 
+for i in range(len(a)):
+    target = T - a[i]
+    l = bisect_left(b, target)
+    r = bisect_right(b, target)
     result += r - l
 print(result)
